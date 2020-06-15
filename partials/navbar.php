9d0a9c13a479790831b73db13b128cd9fc805427
Girard lucas <pro.lucas.girard@gmail.com>
2020-06-15 17:02:00 +0200

    card ok, fiche-produit loading

--- partials/navbar.php
+++ partials/navbar.php
@@ -9,13 +9,13 @@ include 'head.php';
     <span class="navbar-toggler-icon"></span>
   </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
         <ul class="navbar-nav ">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="../view/accueil.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Voyage</a>
+            <a class="nav-link" href="../view/fiche-produit.php">Voyage</a>
         </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -28,12 +28,13 @@ include 'head.php';
             <a class="dropdown-item" href="#">Something else here</a>
             </div>
         </li>
-        <svg class="login " width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        
+    </div>
+    <svg class="login " width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
             <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
             <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
         </svg>
-    </div>
 </nav>
 
 
--- view/accueil.php
+++ view/accueil.php
@@ -46,14 +46,16 @@
         
         <div class="row">
             <?php  for ($i=1; $i <11; $i++):?>
-                <div class="firstcard shadow-sm " style="width: 18rem;">
+                <div class="firstcard shadow-sm " style="width: 18rem;" data-card="cardTrip">
                     <img src="../assets/images/plages.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">JAPON</h5>
-                            <p class="card-text">1/5 NOTE------------------PRIX:</p>
+                            <p class="card-text">1/5 NOTE
+                                <br>PRIX: 200 euro</p>
+                                <a href="#" class="btn btn-primary">INFO</a>
                        </div>
                 </div>
-                <?php endfor ?>5
+                <?php endfor ?>
         </div>
     </div>
 <!-- FIN BOUCLE PHP -->
@@ -72,6 +74,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 </body>
 </html>
 
