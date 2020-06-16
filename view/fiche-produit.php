ebf2b35c15d83ec91c04055c22cb79cf4a5bc4fb
Girard lucas <pro.lucas.girard@gmail.com>
2020-06-16 09:08:30 +0200

    ajout fiche-produit

--- view/fiche-produit.php
+++ view/fiche-produit.php
@@ -23,7 +23,7 @@
 <h1 class="pays text-center">JAPON</h1>
 <br>
 <div class="container">
-    <div class="card mb-8" style="max-width: 540px;">
+    <div class="card mb-12" style="max-width: 540px;">
         <div class="row no-gutters">
             <div class="col-md-4">
                 <img src="../assets/images/plages.jpg" class="card-img" alt="...">
@@ -42,9 +42,7 @@
 
     
 <?php include '../partials/footer.php'?>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
