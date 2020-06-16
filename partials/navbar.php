9e9f07ce0bc620b75ae2c5ef9485a60009c58439
Girard lucas <pro.lucas.girard@gmail.com>
2020-06-16 12:30:11 +0200

    ajout admin

--- partials/navbar.php
+++ partials/navbar.php
@@ -22,7 +22,7 @@ include 'head.php';
             Dropdown
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="../view/admin.php">Action</a>
             <a class="dropdown-item" href="#">Another action</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#">Something else here</a>
