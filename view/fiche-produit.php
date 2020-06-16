153a357e9f9720847c904c09d2c36ae267fd8a41
Girard lucas <pro.lucas.girard@gmail.com>
2020-06-16 09:29:48 +0200

    lucas 16/06 9h30

--- view/fiche-produit.php
+++ view/fiche-produit.php
@@ -23,16 +23,15 @@
 <h1 class="pays text-center">JAPON</h1>
 <br>
 <div class="container">
-    <div class="card mb-12" style="max-width: 540px;">
+    <div class="secondCard mb-12">
         <div class="row no-gutters">
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <img src="../assets/images/plages.jpg" class="card-img" alt="...">
             </div>
-            <div class="col-md-8">
-                <div class="card-body">
+            <div class="col-md-6">
+                <div class="secondCard-body text-center">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
         </div>
